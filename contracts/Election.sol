0c0e62c446d3f7d4400c6d363f0c42bba979c179
Minh Vu <minhd_vu@yahoo.com>
2021-10-22 10:41:15 -0400

    candidate struct solidity

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,27 @@
 pragma solidity >=0.4.2;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    // Read/write Candidates
+    mapping(uint256 => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint256 public candidatesCount;
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 }
